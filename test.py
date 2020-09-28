88a9e3319c40b24306f25f0d5cb43c87f45ab6b1
maxkoeppel <m_koeppel@web.de>
2020-09-28 21:17:30 +0200

    modified tests

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ import pytest
 class Prediction_tests:
 
 	def number_countries(self):
+		countires = './data/all_country_temp_data_CLEAN.csv'
+		fc = './data/country_forecast.csv'
 		assert len(countries['country'].unique())==len(fc['country'].unique())
 
 	def years_of_prediction(self):
+		fc = './data/country_forecast.csv'
 		assert len(fc['year'].unique())==20
