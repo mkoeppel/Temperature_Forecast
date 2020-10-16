35269cf5facb6fe635a5b2fb4240d1481807725a
maxkoeppel <m_koeppel@web.de>
2020-10-16 15:28:46 +0200

    forecast gif added

--- forecast_gif_generation.py
+++ forecast_gif_generation.py
@@ -6,4 +6,4 @@ for i in range(9):
     filename = '~/SPICED/Week_05/bokeh_screenshots/Screenshot_2020-10-16 Bokeh Application{}.png'.format(i)
     images.append(imageio.imread(filename))
 
-imageio.mimsave('./bokeh_forecast.gif', images, fps=5)
+imageio.mimsave('./bokeh_forecast.gif', images, fps=2)
